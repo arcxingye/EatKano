96d0834584982030579095c80ac7b394c15b1592
xy <864903500@qq.com>
2021-02-05 16:27:53 +0800

    标注

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-$link = new mysqli();
+$link = new mysqli('localhost','用户名','密码','数据库名');
 mysqli_set_charset($link, 'utf8');
 if ($link->connect_error) {
     die("连接失败: " . $conn->connect_error);
