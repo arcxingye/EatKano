0ab359e785b7cb3d3f20c35b94ccb64814381fd3
arcxingye <864903500@qq.com>
2021-08-29 22:27:05 +0800

    去除多余

--- SubmitResults.php
+++ SubmitResults.php
@@ -1,19 +1,14 @@
 <?php
 require 'Decipher.php';
 include('conn.php');
-$ciphertext = $_POST['ciphertext'];
-$decipherList = decipher($ciphertext);
+$decipherList = decipher($_POST['ciphertext']);
 $score = $decipherList[0];
-$systeminfo = $decipherList[1];
-$name = $_POST['name'];
-$area = $_POST['area'];
-$message = $_POST['message'];
 $str = "/\ |\/|\~|\!|\@|\#|\\$|\%|\^|\&|\*|\(|\)|\_|\+|\{|\}|\:|\<|\>|\?|\[|\]|\,|\.|\/|\;|\'|\`|\-|\=|\\\|\|/";
-$name = preg_replace($str, "", $name);
-$systeminfo = preg_replace($str, "", $systeminfo);
-$area = preg_replace($str, "", $area);
-$message = preg_replace($str, "", $message);
-if ((strlen($name) <= 30)&&($score < 300)&&($message <= 150)&&(is_numeric($score))&&(is_string($systeminfo))) {
+$name = preg_replace($str, "", $_POST['name']);
+$systeminfo = preg_replace($str, "", $decipherList[1]);
+$area = preg_replace($str, "", $_POST['area']);
+$message = preg_replace($str, "", $_POST['message']);
+if ((strlen($name) <= 30)&&($message <= 150)&&(is_numeric($score))&&(is_string($systeminfo))) {
     $result = mysqli_query($link, "SELECT * FROM ".$ranking." WHERE name='$name' limit 1");
     $data = mysqli_fetch_all($result);
     if ($data) {
