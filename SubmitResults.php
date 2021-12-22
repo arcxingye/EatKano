f4b5289add9bd561a53eade4c1e260f918e14027
arcxingye <864903500@qq.com>
2021-12-23 00:48:10 +0800

    📚我TM又换回来了

--- SubmitResults.php
+++ SubmitResults.php
@@ -7,22 +7,19 @@ $systeminfo = preg_replace($str, "", $_POST['systeminfo']);
 $area = preg_replace($str, "", $_POST['area']);
 $message = preg_replace($str, "", $_POST['message']);
 if ((strlen($name) <= 30)&&($score < 300)&&($message <= 150)&&(is_numeric($score))&&(is_string($systeminfo))) {
-    $record_sql="SELECT id FROM ".$ranking." WHERE name=?";
-    $record_stmt = $link->prepare($record_sql);
-    $record_stmt->bind_param("s",$name);
-    $record_stmt->execute();
-    $rows=$record_stmt->num_rows;
-    $record_stmt->close();
-    if ($rows>0) {
-        $operation_sql = "UPDATE ".$ranking." SET score=?,time=NOW(),systeminfo=?,area=?,message=? WHERE name=?";
+    $result = mysqli_query($link, "SELECT * FROM ".$ranking." WHERE name='$name' limit 1");
+    $data = mysqli_fetch_all($result);
+    if ($data) {
+        if ($score > $data[0][1]) {
+            $sql = "UPDATE ".$ranking." SET score=?,time=NOW(),systeminfo=?,area=?,message=? WHERE name=?";
+        }
     } else {
-        $operation_sql = "INSERT INTO ".$ranking." (score,time,systeminfo,area,message,name) VALUES (?,NOW(),?,?,?,?)";
+        $sql = "INSERT INTO ".$ranking." (score,time,systeminfo,area,message,name) VALUES (?,NOW(),?,?,?,?)";
     }
-    if ($operation_sql) {
-        $operation_stmt = $link->prepare($operation_sql);
-        $operation_stmt->bind_param('issss', $score, $systeminfo, $area, $message, $name);
-        $operation_stmt->execute();
-        $operation_stmt->close();
+    if ($sql) {
+        $stmt = $link->prepare($sql);
+        $stmt->bind_param('issss', $score, $systeminfo, $area, $message, $name);
+        $stmt->execute();
     }
     $link->close();
 }
