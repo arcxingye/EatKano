bd6829426b6eb6938e250251a5ff6ed3ec0c077f
arcxingye <864903500@qq.com>
2021-12-23 11:48:52 +0800

    📚数据库

--- kano.sql
+++ kano.sql
@@ -1,15 +1,12 @@
- SET NAMES utf8 ;
-DROP TABLE IF EXISTS `kano1_rank`;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `kano1_rank` (
+DROP TABLE IF EXISTS `kano_rank`;
+CREATE TABLE `kano_rank` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `score` int(11) NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `time` datetime DEFAULT NULL,
-  `systeminfo` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `area` varchar(255) DEFAULT NULL,
-  `message` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `time` datetime NOT NULL,
+  `system` varchar(255) NOT NULL,
+  `area` varchar(255) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `attempts` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
-LOCK TABLES `kano1_rank` WRITE;
-UNLOCK TABLES;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
