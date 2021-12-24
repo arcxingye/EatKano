b93d7cc75f3c9bd5e27d5598bc6c693358ead145
arcxingye <864903500@qq.com>
2021-12-24 17:54:59 +0800

    ⭐更改目录结构

--- static/main.js
+++ static/main.js
@@ -96,15 +96,15 @@ var _gameBBList = [],
 
 function gameInit() {
     createjs.Sound.registerSound({
-        src: "res/err.mp3",
+        src: "./static/music/err.mp3",
         id: "err"
     });
     createjs.Sound.registerSound({
-        src: "res/end.mp3",
+        src: "./static/music/end.mp3",
         id: "end"
     });
     createjs.Sound.registerSound({
-        src: "res/tap.mp3",
+        src: "./static/music/tap.mp3",
         id: "tap"
     });
     gameRestart();
