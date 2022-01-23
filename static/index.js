3089aa15cade8ffe027c125e66adf13493bbced0
shanguangshenghao <98270203+shanguangshenghao@users.noreply.github.com>
2022-01-24 00:40:14 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,10 +341,10 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
+    if (score <= 49) return '盾牌不重要！';
+    if (score <= 99) return '还有一件事！';
+    if (score <= 149) return '要用魔法打败魔法！';
+    if (score <= 199) return '过来抱抱老爹~';
     return '人？';
 }
 
