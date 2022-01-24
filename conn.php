72a29dd03f4425220247d2b55ea04d33a773f0b2
2783741184 <98264728+2783741184@users.noreply.github.com>
2022-01-24 09:39:11 +0800

    Delete conn.php

--- conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-$link = new mysqli('localhost','用户名','密码','数据库名');
-mysqli_set_charset($link, 'utf8');
-if ($link->connect_error) {
-    die("连接失败: " . $conn->connect_error);
-}
-$ranking = "kano_rank";//排行榜表名
