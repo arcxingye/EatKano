5aa70b4ef4f2b39ecc2602eb8c983a554210587b
kaguya <98514426+kaguya233@users.noreply.github.com>
2022-01-27 16:45:43 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -425,10 +425,10 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
+        if (cps <= 5) return '革命未能成功,同志仍需努力';
+        if (cps <= 8) return '只差一步!';
+        if (cps <= 10)  return '心跟黨走!';
+        if (cps <= 15) return '成功!';
         return '人？';
     }
 
