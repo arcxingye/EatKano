edc03704b5658e6edabbd9f9346d3fd28a2f90d1
chengtx809 <103792951+chengtx809@users.noreply.github.com>
2022-11-30 09:10:13 +0800

    Delete conn.php

--- conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Change this to your own configuration
-$link = new mysqli('localhost','NAME','PASSWORD','kano');
-mysqli_set_charset($link, 'utf8');
-if ($link->connect_error) {
-    die("Failed to connect: " . $conn->connect_error);
-}
-$ranking = "kano_rank";
