9b804e960ec361e91495993d6c0ddb894ec1fd36
fgfobdpqjs <142361890+fgfobdpqjs@users.noreply.github.com>
2024-03-06 17:45:45 +0800

    Add files via upload

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
 // Change this to your own configuration
-$link = new mysqli('localhost','NAME','PASSWORD','kano');
+$link = new mysqli('localhost','NAME','PASSWORD','wangye');
 mysqli_set_charset($link, 'utf8');
 if ($link->connect_error) {
     die("Failed to connect: " . $conn->connect_error);
 }
-$ranking = "kun_rank";
+$ranking = "wangye_rank";
